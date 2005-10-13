c89464a3070f2c8f8973e3d24b3449e6334237bc
icculus <icculus@d67a7ff0-0d32-0410-8e92-b5b47b70abfd>
2005-10-13 02:24:08 +0000

    Patch from Mike Frysinger of Gentoo project to add sanity checks to autogen.sh.

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 #
 
+die() {
+	echo "'$0' failed to run properly"
+	exit 1
+}
+
 set -e
 
-aclocal $ACLOCAL_FLAGS
-automake --foreign
-autoconf
+aclocal $ACLOCAL_FLAGS || die aclocal
+libtoolize -c -f || die libtoolize
+automake --foreign || die automake
+autoconf || die autoconf
 
 #./configure $*
 echo "Now you are ready to run ./configure"
